f6d75e1fe33c6b1e3227f8966c09edde0961e615
Priya Dhanalakota <89081086+Priya-661@users.noreply.github.com>
2024-04-24 11:16:32 +0530

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -2,10 +2,6 @@ def c_to_f(c):
     f = (c * 9/5) + 32
     return f
 
-def main():
-    c = float(input("Enter the temperature in C: "))
-    f = c_to_f(c)
-    print("The equivalent temperature in F is:", f)
-
-if __name__ == "__main__":
-    main()
+c = float(input("Enter temperature in C: "))
+f = c_to_f(c)
+print("Equivalent temperature in F:", f)
