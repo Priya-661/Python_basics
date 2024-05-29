71bd1de05f2f75b21823548074d51d42706b3d23
Priya Dhanalakota <89081086+Priya-661@users.noreply.github.com>
2024-05-29 13:29:48 +0530

    Delete Assign2 directory

--- Assign2/Task1.py
+++
@@ -1,8 +0,0 @@
-#Calculate the sum of all integers in the given list
-def sum_of_integers(int_list):
-    return sum(int_list)
-
-#Example usage
-numbers = [8,3,4,8,8,0,3,5,0,5]
-total = sum_of_integers(numbers)
-print("The sum of all the integers in the list:", total)
